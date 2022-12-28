73d3ed347ebcf8c460ae3cfaa19442468cf75745
Armelini <121030607+Armelini@users.noreply.github.com>
2022-12-28 15:47:07 -0300

    Update notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -16,3 +16,25 @@ let atletas = [
         notas: [10, 10, 10, 9, 9.5]
     }
 ];
+
+// Percorrer todos os itens da matriz de objetos usando o loop "for"
+for (let i = 0; i < atletas.length; i++) {
+    // Obter o objeto atual
+    let atleta = atletas[i];
+
+    // Ordenar as notas do atleta
+    atleta.notas.sort();
+
+    // Eliminar a maior e a menor nota
+    let notasConsideradas = atleta.notas.slice(1, atleta.notas.length - 1);
+
+    // Calcular a média do atleta
+    let soma = 0;
+    notasConsideradas.forEach(function (nota) {
+        soma += nota;
+    });
+    let media = soma / notasConsideradas.length;
+
+    // Mostrar o resultado
+    console.log(`Atleta: ${atleta.nome}\nNotas Obtidas: ${atleta.notas}\nMédia Válida: ${media}\n`);
+}
